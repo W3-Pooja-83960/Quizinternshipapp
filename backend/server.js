4980330d18e5033c6e3f40d887e1fbc65b1e2d6f
W2-bhushan-84039 <bhushanmatte2@gmail.com>
2025-08-26 09:48:22 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,42 @@
-// server.js
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
+
+const pool = require("./config/db"); // ✅ added
+
 const moduleRoutes = require("./routes/moduleRoutes");
 const batchcourseRoutes = require("./routes/batchcourseRoutes");
-
-=======
->>>>>>> sayali
-const { PORT } = require("./config");
+const { PORT } = require("./config/index");
 const routeNotFound = require("./middlewares/routeNotFound");
 const batchRoutes = require("./routes/batch");
 const courseRoutes = require("./routes/course");
-
+const questionRoutes = require("./routes/questions");
+const quizRoutes = require("./routes/quiz");
+const adminRoutes = require("./routes/admin");
 // middlewares
 app.use(express.json());
 
 // routes
-<<<<<<< HEAD
-app.use("/module", moduleRoutes);
 app.use("/batch_course", batchcourseRoutes);
-
-=======
-app.use("/batch",batchRoutes)
-app.use("/course",courseRoutes);
->>>>>>> sayali
-
+app.use("/questions", questionRoutes);
+app.use("/module", moduleRoutes);
+app.use("/batch", batchRoutes);
+app.use("/course", courseRoutes);
+app.use("/api/quiz", quizRoutes);
+app.use("/admin", adminRoutes);
 // route not found
-app.use("/routeNotFound",routeNotFound);
+app.use("/routeNotFound", routeNotFound);
 
+// Test DB connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error(" Database connection failed:", err.message);
+  } else {
+    console.log(" Database connected successfully!");
+    connection.release();
+  }
+});
 
 // Start server
-
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
 });
-
-
