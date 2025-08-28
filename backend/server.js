f50456306c07dcf46ea5a4aa953568bc186471e3
W3-Pooja-83960 <poojamahajan1205@gmail.com>
2025-08-28 13:21:02 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
-// server.js
 const express = require("express");
 const app = express();
 const moduleRoutes = require("./routes/moduleRoutes");
 const batchcourseRoutes = require("./routes/batchcourseRoutes");
-const adminRoutes = require("./routes/admin_Routes")
-
+const optionsRoutes = require("./routes/optionsRoutes");
+const course_moduleRoutes = require("./routes/course_moduleRoutes");
+const admin_Routes = require("./routes/admin_Routes");
 const { PORT } = require("./config");
 const routeNotFound = require("./middlewares/routeNotFound");
-const batchRoutes = require("./routes/batch");
-const courseRoutes = require("./routes/course");
 
 // middlewares
 app.use(express.json());
@@ -16,18 +14,14 @@ app.use(express.json());
 // routes
 app.use("/module", moduleRoutes);
 app.use("/batch_course", batchcourseRoutes);
+app.use("/options", optionsRoutes);
+app.use("/course_module", course_moduleRoutes);
+app.use("/student_batch",admin_Routes);
+app.use("/staff",staff_Routes);
 
-app.use("/batch",batchRoutes)
-app.use("/course",courseRoutes);
-app.use("/student_course", adminRoutes)
 // route not found
-app.use("/routeNotFound",routeNotFound);
-
-
-// Start server
+app.use(routeNotFound);
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+  console.log(`Server Started at http://localhost:${PORT}`);
 });
-
-
