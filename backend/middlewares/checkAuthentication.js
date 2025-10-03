1d764c85fbe362330152068281ca92bda3f54112
sayali bhadane <sayaleebhadane@gmail.com>
2025-10-03 23:55:16 +0530

    updated middlewares folder

--- backend/middlewares/checkAuthentication.js
+++ backend/middlewares/checkAuthentication.js
@@ -3,21 +3,23 @@ const { SECRET_KEY } = require("../config");
 const { errorResponse } = require("../utils/apiResponse");
 
 const checkAuthentication = (request, response, next) => {
-  if (request.url === "/user/register" || request.url === "/user/login") {
-    return next();
-  }
+  if (request.url.startsWith("/user/register") || request.url.startsWith("/user/login")) {
+  return next();
+}
+
 
   const authToken = request.headers.authorization;
   if (!authToken) {
-    return response.send(errorResponse("Token is Missing!"));
+    return response.status(401).send(errorResponse("Token is Missing!"));
   }
 
   try {
+   
     const token = authToken.split(" ")[1];
-    // console.log("token: ", token);
+     console.log("token: ", token);
 
     const decodedToken = jwt.verify(token, SECRET_KEY);
-    // console.log("decodedToken: ", decodedToken);
+    console.log("decodedToken: ", decodedToken);
 
     request.user = decodedToken;
     // console.log("user: ", request.user);
@@ -72,13 +74,25 @@ const checkMentorRole = (request, response, next) => {
   return response.send(errorResponse("UnAuthorized Access! Mentor only"));
 };
 
+//Student middleware
+const checkStudentRole = (request, response, next) => {
+  // check if role is student
+  // if yes then allow the request
+
+  if (request.user.role === "student") {
+    return next();
+  }
+
+  // if no then send an error message
+  return response.send(errorResponse("UnAuthorized Access! Student only"));
+};
+
 // Multi-role middleware
 const checkRoles = (roles = []) => {
   return (req, res, next) => {
     if (!req.user) return res.status(401).send(errorResponse("Not authenticated"));
-    return next();
-    // if (roles.includes(req.user.role)) return next();
-    // return res.status(403).send(errorResponse(`UnAuthorized Access! Only [${roles.join(", ")}]`));
+    if (roles.includes(req.user.role)) return next();
+    return res.status(403).send(errorResponse(`UnAuthorized Access! Only [${roles.join(", ")}]`));
   };
 };
 
@@ -87,5 +101,6 @@ module.exports = {
   checkAdminRole,
   checkCoordinatorRole,
   checkMentorRole,
+  checkStudentRole,
   checkRoles
 };
