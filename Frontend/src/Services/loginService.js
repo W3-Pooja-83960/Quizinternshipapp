c6d020bdd5178636d402a6ebc1dc65194c9aa651
sayali bhadane <sayaleebhadane@gmail.com>
2025-10-04 17:57:22 +0530

    updated service folder-login,quiz,staff files

--- Frontend/src/Services/loginService.js
+++ Frontend/src/Services/loginService.js
@@ -13,41 +13,19 @@ async function register(userData) {
 }
 
 // Login user
-// async function login(email, password) {
-//   try {
-//     const response = await axios.post(`${BASE_URL}/user/login`, { email, password });
-    
-//     // Check if login was successful
-//     if (response.data.status === "success" && response.data.data?.token) {
-//       const token = response.data.data.token;
-//       localStorage.setItem("token", token);  // <-- Save token
-//       console.log("Token saved:", token);
-//     }
-
-//     return response.data; // { status, data: { user, token }, message }
-//   } catch (error) {
-//     console.error("Login failed:", error);
-//     return { status: "error", message: error.message };
-//   }
-// }
-
-
 async function login(email, password) {
   try {
     const response = await axios.post(`${BASE_URL}/user/login`, { email, password });
-    return response.data; // let the component decide what to do with data
+    return response.data; 
   } catch (error) {
     console.error("Login failed:", error);
     return { status: "error", message: error.message };
   }
 }
 
-// const loginServices = {
-//   register,
-//   login
-// };
 
-// export default loginServices;
-
-const loginServices = { login };
+const loginServices = { 
+                        login
+                      };
+                      
 export default loginServices;
